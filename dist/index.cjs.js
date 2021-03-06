6727637d0303c2187a47d028ba5b59b912405603
heyjin20 <happyaws201201@gmail.com>
2021-03-07 05:38:02 +0900

    change svg icon

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -288,12 +288,9 @@ var Icon$d = function (props) {
 
 var Icon$e = function (props) {
     return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 25 24" }, props),
-        React__default['default'].createElement("path", { d: "M19.2 3H5.19995C4.09995 3 3.19995 3.9 3.19995 5V19C3.19995 20.1 4.09995 21 5.19995 21H19.2C20.3 21 21.2 20.1 21.2 19V5C21.2 3.9 20.3 3 19.2 3ZM19.2 19H5.19995V5H19.2V19Z", fill: "#1FC7D4" }),
-        React__default['default'].createElement("path", { d: "M11.45 7.72021H6.44995V9.22022H11.45V7.72021Z", fill: "#1FC7D4" }),
-        React__default['default'].createElement("path", { d: "M18.2 15.75H13.2V17.25H18.2V15.75Z", fill: "#1FC7D4" }),
-        React__default['default'].createElement("path", { d: "M18.2 13.25H13.2V14.75H18.2V13.25Z", fill: "#1FC7D4" }),
-        React__default['default'].createElement("path", { d: "M8.19995 18H9.69995V16H11.7V14.5H9.69995V12.5H8.19995V14.5H6.19995V16H8.19995V18Z", fill: "#1FC7D4" }),
-        React__default['default'].createElement("path", { d: "M14.29 10.95L15.7 9.54L17.11 10.95L18.17 9.89L16.76 8.47L18.17 7.06L17.11 6L15.7 7.41L14.29 6L13.23 7.06L14.64 8.47L13.23 9.89L14.29 10.95Z", fill: "#1FC7D4" })));
+        React__default['default'].createElement("g", { fill: "none", "fill-rule": "evenodd" },
+            React__default['default'].createElement("g", { fill: "#000" },
+                React__default['default'].createElement("path", { d: "M7.24.876C7.182.535 6.9.277 6.555.252 6.21.227 5.892.441 5.786.77l-2.09 6.48H1c-.414 0-.75.336-.75.75s.336.75.75.75h3.242c.326 0 .614-.21.714-.52L6.27 4.16l1.84 10.965c.058.346.347.605.697.625.35.02.667-.206.764-.542l2.429-8.48 1.28 4.48c.082.288.329.5.627.537.298.038.59-.106.741-.365l1.54-2.629h1.166c.327 1.154 1.388 2 2.647 2 1.519 0 2.75-1.231 2.75-2.75S21.519 5.25 20 5.25c-1.259 0-2.32.846-2.646 2h-1.596c-.267 0-.513.141-.647.37l-.872 1.488-1.518-5.314c-.092-.322-.386-.544-.72-.544-.335 0-.63.222-.722.543L9.04 11.608 7.24.876zM18.75 8c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25z", transform: "translate(1 4)" })))));
 };
 
 var Icon$f = function (props) {
